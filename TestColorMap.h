3dec82e868c6c585ead5ba7a567ea978b93280f6
320191579 <320191579@INGBTCPIC5NB313>
2024-08-07 09:13:36 +0530

    added CMakeList file

---
+++ TestColorMap.h
@@ -0,0 +1,14 @@
+#ifndef TEST_COLOR_MAP_H
+#define TEST_COLOR_MAP_H
+
+#include <vector>
+#include <string>
+#include <gtest/gtest.h>
+#include "ColorMap.h"
+
+class ColorMapTest : public ::testing::Test {
+public:
+    ColorMap colorMap;
+};
+
+#endif // TEST_COLOR_MAP_H
