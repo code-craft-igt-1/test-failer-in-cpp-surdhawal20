7652e2edc326aa6afaf99087d7e61ff7bbb32ecd
320191579 <320191579@INGBTCPIC5NB313>
2024-08-22 11:43:49 +0530

    Fix the build error

--- TshirtsSize.cpp
+++ TshirtsSize.cpp
@@ -1,18 +1,17 @@
 #include "TshirtsSize.h"
 
 class TshirtSize {
-public:
-    // Function to get the size based on cms
-    char getSizeInCms(int sizeInCms) const {
-        if (sizeInCms < 38) {
-            return 'S';
+
+    public:
+        // Function to get the size based on cms
+        char getSizeInCms(int sizeInCms) const {
+            if (sizeInCms < 38) {
+                return 'S';
+            }else if (sizeInCms <= 42) {  // This simplifies the logic for M
+                return 'M';
+            }else {
+                return 'L';
+            }
         }
-        else if (sizeInCms <= 42) {  // This simplifies the logic for M
-            return 'M';
-        }
-        else {
-            return 'L';
-        }
-    }
 };
 
