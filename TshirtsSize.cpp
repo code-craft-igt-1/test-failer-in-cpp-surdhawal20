1561bd35926c8839c458e0f481a0e4fa5ae9f7c6
320191579 <320191579@INGBTCPIC5NB313>
2024-08-22 12:01:49 +0530

    Issues fixes

--- TshirtsSize.cpp
+++ TshirtsSize.cpp
@@ -1,16 +1,14 @@
 #include "TshirtsSize.h"
 
-class TshirtSize {
- public:
-    // Function to get the size based on cms
-    char getSizeInCms(int sizeInCms) const {
-        if (sizeInCms < 38) {
-            return 'S';
-        } else if (sizeInCms <= 42) {  // This simplifies the logic for M
-            return 'M';
-        } else {
-            return 'L';
-        }
+// Function to get the size based on cms
+char getSizeInCms(int sizeInCms) {
+    if (sizeInCms < 38) {
+        return 'S';
+    } else if (sizeInCms <= 42) {  // This simplifies the logic for M
+        return 'M';
+    } else {
+        return 'L';
     }
-};
+}
+
 
--- TshirtsSize.h
+++ TshirtsSize.h
@@ -1,12 +1,7 @@
-// TshirtSize.h
-
 #ifndef TSHIRTSIZE_H
 #define TSHIRTSIZE_H
 
-class TshirtSize {
-public:
-    // Function to get the t-shirt size based on chest circumference in centimeters
-    char getSizeInCms(int sizeInCms) const;
-};
+// Function to get the size based on cms
+char getSizeInCms(int sizeInCms);
 
 #endif // TSHIRTSIZE_H
