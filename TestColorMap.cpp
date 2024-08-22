b191a7e5c846769527cb0b33cea1d753d69103c4
320191579 <320191579@INGBTCPIC5NB313>
2024-08-22 12:22:43 +0530

    issue fix

--- TestColorMap.cpp
+++ TestColorMap.cpp
@@ -8,7 +8,7 @@
 std::string generateExpectedOutput(const ColorMap& colorMap) {
     std::vector<std::pair<std::string, std::string>> colorPairs = colorMap.getColorPairs();
     std::ostringstream colorPairStream;
-    for (size_t i = 1; i < colorPairs.size(); ++i) {
+    for (size_t i = 1; i <= colorPairs.size(); ++i) {
         colorPairStream << i << " , "
             << colorPairs[i].first << " , "
             << colorPairs[i].second << '\n';
